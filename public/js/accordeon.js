c37b0df5a5ebe8ffe70c14be07a36e9ec1095684
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2024-08-26 11:24:49 -0400

    Modification dynamique page emplois

--- public/js/accordeon.js
+++ public/js/accordeon.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  const jobElements = document.querySelectorAll(".projet.building.emplois-demander");
+  const currentLang = document.documentElement.getAttribute('lang');
 
+  // Select only the job elements that match the current language
+  const jobElements = document.querySelectorAll(`.projet.building.emplois-demander[lang="${currentLang}"]`);
+  
   // Count the number of elements
   const jobCount = jobElements.length;
-
+  
   // Insert the count into the <span> elements with class "jobCount"
   document.querySelectorAll(".jobCount").forEach((span) => {
     span.textContent = jobCount;
   });
-
+  
   const checkboxs = document.querySelectorAll(".state");
   let conteneur_checkboxs = document.querySelectorAll(".conteneur_checkbox_projet");
 
