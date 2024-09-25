fccf808bad1366e8ec79e2cfc9051b3f2c0ad4bd
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2024-09-25 12:20:08 -0400

    changement majeurs i18n

--- public/js/langue.js
+++ public/js/langue.js
@@ -1,79 +1,79 @@
-function setLanguage(lang) {
-  // Define the allowed languages
-  const allowedLanguages = ["en", "fr"];
+// function setLanguage(lang) {
+//   // Define the allowed languages
+//   const allowedLanguages = ["en", "fr"];
 
-  // Check if the provided lang is in the allowed languages, otherwise default to 'en'
-  if (!allowedLanguages.includes(lang)) {
-    lang = "en";
-  }
+//   // Check if the provided lang is in the allowed languages, otherwise default to 'en'
+//   if (!allowedLanguages.includes(lang)) {
+//     lang = "en";
+//   }
 
-  // Store the language preference in local storage
-  localStorage.setItem("preferredLanguage", lang);
+//   // Store the language preference in local storage
+//   localStorage.setItem("preferredLanguage", lang);
 
-  // Update the lang attribute on the <html> tag
-  document.documentElement.setAttribute("lang", lang);
+//   // Update the lang attribute on the <html> tag
+//   document.documentElement.setAttribute("lang", lang);
 
-  // Update the URL to include the language query parameter only if it differs
-  const url = new URL(window.location.href);
-  const currentLang = url.searchParams.get("lang");
+//   // Update the URL to include the language query parameter only if it differs
+//   const url = new URL(window.location.href);
+//   const currentLang = url.searchParams.get("lang");
 
-  // Only update the URL if the lang parameter is different
-  if (currentLang !== lang) {
-    url.searchParams.set("lang", lang);
-    window.history.replaceState({}, "", url.toString());
-  }
-}
+//   // Only update the URL if the lang parameter is different
+//   if (currentLang !== lang) {
+//     url.searchParams.set("lang", lang);
+//     window.history.replaceState({}, "", url.toString());
+//   }
+// }
 
-function getLanguage() {
-  // Get the language preference from local storage
-  const lang = localStorage.getItem("preferredLanguage") || "en"; // Default to 'en' if no preference is found
+// function getLanguage() {
+//   // Get the language preference from local storage
+//   const lang = localStorage.getItem("preferredLanguage") || "en"; // Default to 'en' if no preference is found
 
-  // Define the allowed languages
-  const allowedLanguages = ["en", "fr"];
+//   // Define the allowed languages
+//   const allowedLanguages = ["en", "fr"];
 
-  // Return the language if it's allowed, otherwise return 'en'
-  return allowedLanguages.includes(lang) ? lang : "en";
-}
+//   // Return the language if it's allowed, otherwise return 'en'
+//   return allowedLanguages.includes(lang) ? lang : "en";
+// }
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  const lang = getLanguage();
+// document.addEventListener("DOMContentLoaded", () => {
+//   const lang = getLanguage();
 
-  document.documentElement.setAttribute("lang", lang);
-  setLanguage(lang);
-  document.documentElement.style.setProperty(
-    "--scrollbar-width",
-    window.innerWidth - document.documentElement.clientWidth + "px"
-  );
+//   document.documentElement.setAttribute("lang", lang);
+//   setLanguage(lang);
+//   document.documentElement.style.setProperty(
+//     "--scrollbar-width",
+//     window.innerWidth - document.documentElement.clientWidth + "px"
+//   );
 
-  const btnFr = document.querySelector(".choix_langue_fr");
-  const btnEn = document.querySelector(".choix_langue_en");
+//   const btnFr = document.querySelector(".choix_langue_fr");
+//   const btnEn = document.querySelector(".choix_langue_en");
 
-  btnFr.addEventListener("click", () => {
-    setLanguage("fr");
-    console.log("click");
+//   btnFr.addEventListener("click", () => {
+//     setLanguage("fr");
+//     console.log("click");
 
-    btnEn.classList.remove("active");
-    btnFr.classList.add("active");
-  });
+//     btnEn.classList.remove("active");
+//     btnFr.classList.add("active");
+//   });
 
-  btnEn.addEventListener("click", () => {
-    setLanguage("en");
-    console.log("click");
+//   btnEn.addEventListener("click", () => {
+//     setLanguage("en");
+//     console.log("click");
 
-    btnFr.classList.remove("active");
-    btnEn.classList.add("active");
-  });
+//     btnFr.classList.remove("active");
+//     btnEn.classList.add("active");
+//   });
 
-  console.log("patate");
-  console.log(lang);
+//   console.log("patate");
+//   console.log(lang);
 
-  // Set the active class based on the current language
-  if (lang === "fr") {
-    btnEn.classList.remove("active");
-    btnFr.classList.add("active");
-  } else {
-    btnFr.classList.remove("active");
-    btnEn.classList.add("active");
-  }
-});
+//   // Set the active class based on the current language
+//   if (lang === "fr") {
+//     btnEn.classList.remove("active");
+//     btnFr.classList.add("active");
+//   } else {
+//     btnFr.classList.remove("active");
+//     btnEn.classList.add("active");
+//   }
+// });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,52 @@
+export const showDefaultLang = false;
+
+export const languages = {
+    en: 'EN',
+    fr: 'FR',
+  };
+  
+  export const defaultLang = 'fr';
+  
+  export const ui = {
+    en: {
+        'nav.a-propos': 'About',
+        'nav.alarme': 'Fire alarm', 
+        'nav.assc': 'Ups',
+        'nav.contact':'Contact',
+        'nav.emplois':'Careers',
+      'nav.home': 'index',
+      'nav.projets':'Projects',
+    
+    },
+    fr: {
+        'nav.a-propos': 'À propos',
+        'nav.alarme': 'Alarme incendie', 
+        'nav.assc': 'Assc',
+        'nav.contact':'Contact',
+        'nav.emplois':'Emplois',
+      'nav.home': 'Index',
+      'nav.projets':'Projets',
+    },
+  } as const;
+
+
+  export const routes = {
+    en: {
+        'index': 'index',
+        'a-propos': 'about',
+        'alarme': 'alarm',
+        'assc': 'ups',
+        'contact':'contact',
+        'emplois':'careers',
+        'projets':'projects'
+    },
+    fr: {
+        'index': 'index',
+        'a-propos': 'a-propos',
+        'alarme': 'alarme',
+        'assc': 'assc',
+        'contact':'contact',
+        'emplois':'emplois',
+        'projets':'projets'
+    },
+}
